9d245259984c5f33e6502ab7197b3385c4e83941
Ndivho69 <ndibo69@gmail.com>
2023-07-19 21:50:38 +0200

    Configured swagger and moved to MVC Pattern

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,25 @@
 import express from "express";
-import "dotenv/config";
-import swaggerDocs from './swagger.js'
-import user_router from "./routes/user_routes.js";
-import pool from "./configuration/database_configuration.js";
 import cors from 'cors'
 import bodyParser from "body-parser";
+import "dotenv/config";
+
+
+import swaggerDocs from './configuration/swagger.js'
+import user_router from "./routes/user_routes.js";
+import pool from "./configuration/database_configuration.js";
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.get("/", (request, response) => {
   response.send({ message: "Welcome to BankLingo!" });
 });
+
 app.listen(PORT, () => {
   console.log(`Listening to Port ${PORT}`);
   swaggerDocs(app, PORT)
 });
+
 const corsOptions = {
   origin: "*",
 };
--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -2,6 +2,48 @@ import createUser from "../controllers/user_controller.js";
 import express from "express";
 const user_router = express.Router();
 
+
+
+/**
+ * @openapi
+ * '/api/createuser':
+ *  post:
+ *     tags:
+ *     - User Route
+ *     summary: Create a User
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - id
+ *              - name
+ *            properties:
+ *              name:
+ *                type: string
+ *                default: string
+ *              surnaname:
+ *                type: string
+ *                default: string
+ *              age:
+ *                 type: number
+ *                 default: 0
+ *              email:
+ *                 type: string
+ *                 default: string
+ *              password:
+ *                  type: string
+ *                  default: string
+ *     responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ */
 user_router.post("/", createUser);
 
 
