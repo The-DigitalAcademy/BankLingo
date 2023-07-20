86b68daa0a211e53c0763648256756203aa41e2c
Prince-Kay99 <ph.kubaye@gmail.com>
2023-07-20 16:30:03 +0200

    Home ui done

---
+++ Frontend/src/app/pages/lesson-plan-calender/lesson-plan-calender.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-plan-calender',
+  templateUrl: './lesson-plan-calender.component.html',
+  styleUrls: ['./lesson-plan-calender.component.scss']
+})
+export class LessonPlanCalenderComponent {
+
+}
