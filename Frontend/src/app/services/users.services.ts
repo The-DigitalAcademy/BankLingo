99933ba5ca9b9d7a828a689347dbdb968f0ccbf9
Pamela <pamelarikamakamu@gmail.com>
2023-07-24 14:15:56 +0200

    forget password and otp done

--- Frontend/src/app/services/users.services.ts
+++ Frontend/src/app/services/users.services.ts
@@ -28,6 +28,35 @@ login(credentials: { email: string, password: string }): Observable<any> {
 }
 
 
+  // sendOTP(email: string): Observable<any> {
+  //   const url = `${this.apiUrls}/api/sendOTP`;
+  //   return this.http.post(url, { email });
+  // }
 
+  updatePassword(email: string, otp: number, password: string): Observable<any> {
+    const url = `${this.apiUrls}/api/user/update-password`;
+    return this.http.post(url, { email, otp, password });
+  }
   
+
+  sendPasswordResetOTP(email: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrls}/api/user/sendOTP`, {
+      email: email,
+    });
+  }
+
+  verifyOTP(email: string, otp: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrls}/api/user/verify-otp`, {
+      email: email,
+      otp: otp,
+    });
+  }
+
+  resetPassword(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrls}/api/user/passwordReset`, {
+      email: email,
+      password: password,
+    });
+  }
 }
+
