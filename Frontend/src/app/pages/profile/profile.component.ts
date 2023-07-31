15c83b813ca68d53d45bf5742b94956f847e77cb
Altharia <althariam@gmail.com>
2023-07-31 11:41:43 +0200

    profile

--- Frontend/src/app/pages/profile/profile.component.ts
+++ Frontend/src/app/pages/profile/profile.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UsersService } from 'src/app/services/users.services';
 import { Router } from '@angular/router';
 
+
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+import { Users } from 'src/app/types/users';
+
+import { HttpClient } from '@angular/common/http';
+import { SessionsService } from 'src/app/services/sessions.service';
+import Swal from 'sweetalert2';
+
+const URL = 'http://localhost:4500';
 
 @Component({
   selector: 'app-profile',
@@ -20,13 +36,44 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent {
 
+  user!: any;
+  profileForm!: FormGroup;
+  
+
   progressValue = 35;
   maxValue = 100;constructor(
     private usersService: UsersService,
-    private router: Router
+    private router: Router, private formBuilder: FormBuilder,
+    
+    private session: SessionsService,
   ) {}
 
 
+  
+  ngOnInit() {
+    // Retrieve the user data from session storage
+    this.user = this.session.getLoggedUser();
+
+    // Check if the user variable contains valid user data before initializing the form
+    if (this.user && Object.keys(this.user).length > 0) {
+      this.initializeForm();
+    } else {
+      // Handle the case when the user data is not available
+      console.log('User data not found in session storage');
+      // You can take appropriate actions, such as redirecting the user to the login page.
+    }
+  }
+
+  initializeForm() {
+    this.profileForm = this.formBuilder.group({
+      name: [this.user.name, Validators.required],
+      surname: [this.user.surname, Validators.required],
+      age: [this.user.age, Validators.required],
+      contact_number: [this.user.contact_number],
+      email: [this.user.email, [Validators.required, Validators.email]],
+      profile_picture: [this.user.profile_picture],
+    });
+  }
 
   // Update the progressValue as needed (e.g., based on an event or timer)
   updateProgress() {
