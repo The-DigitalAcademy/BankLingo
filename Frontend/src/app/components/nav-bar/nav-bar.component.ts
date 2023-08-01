bb72ac6461e4957b8282eb4f03e48d3a89923d8a
Prince-Kay99 <ph.kubaye@gmail.com>
2023-08-01 15:55:09 +0200

    Navigate back DONE

--- Frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+
+  constructor(private router: Router, private location : Location) { }
+
+  navigateBack(): void {
+    this.location.back()
+    // this.router.navigate(['/']); // Navigate back to the previous page
+  }
 
 }
