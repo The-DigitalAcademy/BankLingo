c7d6ac40a972ea8a99e724e1a9a760adeaa5d34c
ndivhom <ndibo69@gmail.com>
2023-08-01 15:49:05 +0200

    updates on pwa

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionsService } from './services/sessions.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
-
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+export class AppComponent {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private swUpdate: SwUpdate
+  ) {}
 
   showNavBar(): boolean {
     // Check if the current route is the login page
@@ -20,4 +24,18 @@ export class AppComponent{
     return !isLoginPage;
   }
   title = 'Frontend';
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (
+          confirm(
+            "You're using an old version of the control panel. Want to update?"
+          )
+        ) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
