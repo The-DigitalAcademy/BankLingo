3fc48dd41659802aae667f33435c5104bb0587ef
ndivhom <ndibo69@gmail.com>
2023-08-11 13:28:48 +0200

    topic page done

--- Frontend/src/app/pages/testing/testing.component.ts
+++ Frontend/src/app/pages/testing/testing.component.ts
@@ -15,6 +15,7 @@ export class TestingComponent {
   localTopics: any;
   localParse!: Welcome;
   coveredStatus: boolean = false;
+  isLoading:boolean = false;
   constructor(
     private route: ActivatedRoute,
     private sharedService: SharedService,
@@ -25,9 +26,15 @@ export class TestingComponent {
     });
   }
   ngOnInit() {
+    this.isLoading = true;
     this.core.getTopicsById(100).subscribe({
       next: (data) => {
+        // Instead of storing in localStorage, you can directly process the data here
+        const topicsData = data as unknown as Welcome; // Assuming Welcome is the type of your data
+        this.course = topicsData.topic_description?.course.lessons;
+        this.isLoading = false;
         localStorage.setItem('topics', JSON.stringify(data));
+        console.log(this.course);
       },
       error: (err) => {
         console.log(err);
@@ -36,15 +43,13 @@ export class TestingComponent {
     const routeParam = this.route.snapshot.paramMap;
     const routeId = String(routeParam.get('day'));
 
-    this.localTopics = localStorage.getItem('topics');
-    
-    this.localParse = JSON.parse(this.localTopics) as Welcome;
+ 
+
     
     // if (topicsJSON) {
     //   this.topicsObj = JSON.parse(topicsJSON) as Welcome;
     // }
 
-    this.course = this.localParse.topic_description?.course.lessons;
-    console.log(this.course);
+
   }
 }
