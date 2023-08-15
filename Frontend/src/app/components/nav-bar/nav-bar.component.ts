095e1a126670b283c55e95d25a1d897a642e9c45
Pamela <pamelarikamakamu@gmail.com>
2023-08-15 16:30:49 +0200

    Add commenting

--- Frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -16,12 +16,15 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
     }
 
+    // A method that retrieves the title from another service using "getTitle()"
+// Returns the title as a string
     showTitle() : string{
       return this.titlePage.getTitle()
     }
 
-
+// Function to navigate back to the previous page
   navigateBack(): void {
+    // Utilize the "location" service to navigate back
     this.location.back()
     // this.router.navigate(['/']); // Navigate back to the previous page
   }
