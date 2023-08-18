511b0b05a7f78e8fe53a500503d5a09650c107cd
Prince-Kay99 <ph.kubaye@gmail.com>
2023-08-18 09:45:26 +0200

    Updated bugs

--- Frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { SessionsService } from 'src/app/services/sessions.service';
 
 
 
@@ -12,14 +14,24 @@ import { Title } from '@angular/platform-browser';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor( private location: Location, private titlePage: Title) { }
-  ngOnInit(): void {
+  onProfile=false
+  constructor(private router: Router, private location : Location, private titlePage : Title, private session : SessionsService) { }  ngOnInit(): void {
   }
 
   // A method that retrieves the title from another service using "getTitle()"
   // Returns the title as a string
   showTitle(): string {
     return this.titlePage.getTitle()
+  }
+
+  isProfilePage(): boolean{
+    const isProfilePage = this.router.url === '/profile';
+    return ( isProfilePage );
+  }
+
+  logout(){
+    this.session.clean()
+    this.router.navigate(["/login"])
   }
 
 // Function to navigate back to the previous page
